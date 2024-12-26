6d13ccc4424303daae4bf0a04037067795b8d216
AndNeeeeewSite <pavlopysarenko1234@gmail.com>
2024-12-26 19:05:21 +0200

    !

---
+++ script.js
@@ -0,0 +1,33 @@
+//                        1.
+slider__input = document.querySelector('.slider__input')
+slider__image = document.querySelector('.slider__image')
+size = 300
+slider__input.addEventListener(
+    'input',
+    _.debounce(
+    () => {
+        size = slider__input.value
+        slider__image.style.maxWidth = (size * 3) + 'px'
+    },
+    100,
+    {
+        leading:false,
+        trailing:true,
+    }
+  ) 
+)
+//               2.
+div = document.querySelector('#box')
+document.addEventListener('mousemove',
+    _.debounce(
+        (event) => {
+            x = event.clientX;
+            div.style.marginLeft = x + 'px'
+        },
+        10,
+        {
+            leading:true,
+            trailing:false,
+        }
+    )
+)
